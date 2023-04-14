2f2f031691af97447fa849342165741032df0c4d
Dinh Nguyen Cong Quy <congquyk66hust@gmail.com>
2023-04-14 10:59:36 +0700

    Changed text file in exercise 2.2.2

--- lab01/FirstDialog_2_2_2.java
+++ lab01/FirstDialog_2_2_2.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 public class FirstDialog_2_2_2{
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
         System.exit(0);
     }
 }
